481d0b443428f09e56afd3087a3b86304eeeca23
WayneMusungu <waynemuyera17@gmail.com>
2022-05-14 09:43:47 +0300

    Initial commit

---
+++ run.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+# def index():
+    # return"<h1>Hello World!</h1>"
+    
+
+def index():
+    return render_template("index.html")
+
+@app.route('/user/<name>')
+def user(name):
+    return"<h1>Hello {}!</h1>".format(name)
+
+# Custom Error pages
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('fourOwfour.html'),404
+    
