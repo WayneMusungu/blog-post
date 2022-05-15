7ef5c61b1611202f7cc7190faa961571fb73e0d1
WayneMusungu <waynemuyera17@gmail.com>
2022-05-15 14:20:39 +0300

    Add login form

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from turtle import title
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
@@ -37,9 +37,13 @@ def home():
 def about():
    return render_template("about.html", title='About')
 
-@app.route("/register", )
+@app.route("/register", methods=['GET', 'POST'] )
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
 @app.route("/login")
