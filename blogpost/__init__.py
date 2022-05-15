440f27c8166941c2514e94267ff1b835d7dda05f
WayneMusungu <waynemuyera17@gmail.com>
2022-05-15 19:22:21 +0300

    Install flask_bcrypt

--- blogpost/__init__.py
+++ blogpost/__init__.py
@@ -1,6 +1,7 @@
 # from turtle import title
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 
 
@@ -15,5 +16,7 @@ app.config['SECRET_KEY'] = '975499ff23f5019775351a3cace85d3309ec313ed1ee70897a29
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
+
+bcrypt = Bcrypt(app)
 
 from blogpost import routes
