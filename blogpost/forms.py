ecf70e1960648c1e5b2c13199b959190cee5f67d
WayneMusungu <waynemuyera17@gmail.com>
2022-05-16 08:53:22 +0300

    Create a PostForm

--- blogpost/forms.py
+++ blogpost/forms.py
@@ -1,8 +1,9 @@
 import email
 from click import confirm
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField, SelectField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from blogpost.models import User
 
@@ -45,7 +46,7 @@ class UpdateAccountForm(FlaskForm):
                            validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
-    # picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Update')
 
     def validate_username(self, username):
@@ -59,5 +60,12 @@ class UpdateAccountForm(FlaskForm):
             user = User.query.filter_by(email=email.data).first()
             if user:
                 raise ValidationError('That email is taken. Please choose a different one.')
+            
+            
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    category = SelectField('Categories', choices=[('Business Quotes', 'Business Quotes'), ('Life Quotes', 'Life Quotes'), ('Funny Quotes', 'Funny Quotes'), ('Wisdom Quotes', 'Wisdom Quotes'),('Inspirational Quotes', 'Inspirational Quotes'), ('Controversial Quotes', 'Controversial Quotes')], validators=[DataRequired()])
+    submit = SubmitField('Post')
 
     
