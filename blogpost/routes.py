fe99e4c1da56bc51e49cddd492b2ba306362890f
WayneMusungu <waynemuyera17@gmail.com>
2022-05-16 07:37:25 +0300

    Create an update account form for authenticated users

--- blogpost/routes.py
+++ blogpost/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for, flash, redirect, request
 from blogpost import app, db, bcrypt
-from blogpost.forms import RegistrationForm, LoginForm
+from blogpost.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from blogpost.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -97,5 +97,7 @@ def logout():
 @app.route("/account")
 @login_required
 def account():
+    form = UpdateAccountForm()
+    
     image_file = url_for('static', filename='profile_pics/'+ current_user.image_file)
-    return render_template('account.html', title='Account', image_file=image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
